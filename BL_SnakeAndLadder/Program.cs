2ab4f98a84fb659a71d11aedaa83c58124b04d90
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-18 09:07:44 +0530

    [Shoaib] Adding dice roll feature

--- BL_SnakeAndLadder/Program.cs
+++ BL_SnakeAndLadder/Program.cs
@@ -7,6 +7,10 @@
             Console.WriteLine("Welcome to Snake & Ladder Game\n");
             int plrPos = 0; // Player Position
             Console.WriteLine("Initial position of player is " + plrPos);
+            int diceRoll; // Dice rolling
+            Random random = new Random();
+            diceRoll = random.Next(1, 7);
+            Console.WriteLine("Dice rolled: "+diceRoll);
         }
     }
 }
