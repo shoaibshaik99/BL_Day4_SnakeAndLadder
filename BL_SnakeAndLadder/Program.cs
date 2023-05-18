3f0b4cd66a7cc61d4d2fd1bba935e1971eab5c36
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-18 09:18:58 +0530

    [Shoaib] Adding the 3 options of NoPlay, Ladder and Snake

--- BL_SnakeAndLadder/Program.cs
+++ BL_SnakeAndLadder/Program.cs
@@ -4,13 +4,31 @@
     {
         public static void Main(string[] args)
         {
+            const int NO_PLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
             Console.WriteLine("Welcome to Snake & Ladder Game\n");
             int plrPos = 0; // Player Position
-            Console.WriteLine("Initial position of player is " + plrPos);
+            //Console.WriteLine("Initial position of player is " + plrPos + "\n");
             int diceRoll; // Dice rolling
             Random random = new Random();
             diceRoll = random.Next(1, 7);
-            Console.WriteLine("Dice rolled: "+diceRoll);
+            Console.WriteLine("Dice rolled: " + diceRoll);
+            int option = random.Next(0, 3);//0,1,2
+            switch (option)
+            {
+                case NO_PLAY:
+                    Console.WriteLine("For this dice roll the move is  No Play");
+                    break;
+                case LADDER:
+                    Console.WriteLine("For this dice roll the move is  Ladder");
+                    plrPos = plrPos + diceRoll;
+                    break;
+                case SNAKE:
+                    Console.WriteLine("For this dice roll the move is  Snake");
+                    plrPos = plrPos - diceRoll;
+                    break;
+            }
         }
     }
 }
