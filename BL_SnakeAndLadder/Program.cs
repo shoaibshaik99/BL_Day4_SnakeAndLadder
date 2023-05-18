db9a0a299b7decc40fcf3222888c724568aacdec
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-18 09:00:48 +0530

    [Shoaib] Initiating at position 0

--- BL_SnakeAndLadder/Program.cs
+++ BL_SnakeAndLadder/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake & Ladder Game\n");
+            int plrPos = 0; // Player Position
+            Console.WriteLine("Initial position of player is " + plrPos);
         }
     }
 }
