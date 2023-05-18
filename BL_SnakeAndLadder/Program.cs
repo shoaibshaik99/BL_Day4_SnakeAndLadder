1707e927d6b4b40f7b72bc9aff76891da8098a77
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-18 08:57:57 +0530

    Printing welcome message

---
+++ BL_SnakeAndLadder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BL_SnakeAndLadder
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake & Ladder Game\n");
+        }
+    }
+}
