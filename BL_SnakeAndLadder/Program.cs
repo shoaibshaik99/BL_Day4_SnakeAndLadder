bbf497dda9ce976cb60dad94d45ba88e6c9d50c0
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-18 09:37:50 +0530

    [Shoaib] Ensuring player position is always positive

--- BL_SnakeAndLadder/Program.cs
+++ BL_SnakeAndLadder/Program.cs
@@ -7,28 +7,38 @@
             const int NO_PLAY = 0;
             const int LADDER = 1;
             const int SNAKE = 2;
+            const int MIN_POS = 0;
+            const int MAX_POS = 100;
             Console.WriteLine("Welcome to Snake & Ladder Game\n");
             int plrPos = 0; // Player Position
             //Console.WriteLine("Initial position of player is " + plrPos + "\n");
             int diceRoll; // Dice rolling
             Random random = new Random();
-            diceRoll = random.Next(1, 7);
-            Console.WriteLine("Dice rolled: " + diceRoll);
-            int option = random.Next(0, 3);//0,1,2
-            switch (option)
+            while (plrPos< MAX_POS)
             {
-                case NO_PLAY:
-                    Console.WriteLine("For this dice roll the move is  No Play");
-                    break;
-                case LADDER:
-                    Console.WriteLine("For this dice roll the move is  Ladder");
-                    plrPos = plrPos + diceRoll;
-                    break;
-                case SNAKE:
-                    Console.WriteLine("For this dice roll the move is  Snake");
-                    plrPos = plrPos - diceRoll;
-                    break;
+                diceRoll = random.Next(1, 7);
+                Console.WriteLine("Dice rolled: " + diceRoll);
+                int option = random.Next(0, 3);//0,1,2
+                switch (option)
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("For this dice roll the move is  No Play");
+                        break;
+                    case LADDER:
+                        Console.WriteLine("For this dice roll the move is  Ladder");
+                        plrPos += diceRoll;
+                        break;
+                    case SNAKE:
+                        Console.WriteLine("For this dice roll the move is  Snake");
+                        plrPos -= diceRoll;
+                        if (plrPos < MIN_POS)
+                        {
+                            plrPos = MIN_POS;
+                        }
+                        break;
+                }
             }
+            Console.WriteLine("Player position: " + plrPos + "\n");
         }
     }
 }
