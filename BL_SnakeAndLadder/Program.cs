a23ac7261265d1978173d5c9d7011c87c0f7fb2c
shaikshoaib99 <smdshoaib137@gmail.com>
2023-05-18 10:52:23 +0530

    [Shoaib] priting player position for each roll and also priting total count of rolls at the end

--- BL_SnakeAndLadder/Program.cs
+++ BL_SnakeAndLadder/Program.cs
@@ -9,20 +9,22 @@
             const int SNAKE = 2;
             const int MIN_POS = 0;
             const int MAX_POS = 100;
+            int countOfRolls = 0;
             Console.WriteLine("Welcome to Snake & Ladder Game\n");
             int plrPos = 0; // Player Position
-            //Console.WriteLine("Initial position of player is " + plrPos + "\n");
+            //Console.WriteLine("Initial position of player is " + plrPos);
             int diceRoll; // Dice rolling
             Random random = new Random();
-            while (plrPos< MAX_POS)
+            while (plrPos < MAX_POS)
             {
+                Console.WriteLine("Current player position: "+ plrPos);
                 diceRoll = random.Next(1, 7);
                 Console.WriteLine("Dice rolled: " + diceRoll);
                 int option = random.Next(0, 3);//0,1,2
                 switch (option)
                 {
                     case NO_PLAY:
-                        Console.WriteLine("For this dice roll the move is  No Play\n");
+                        Console.WriteLine("For this dice roll the move is  No Play");
                         break;
                     case LADDER:
                         Console.WriteLine("For this dice roll the move is  Ladder");
@@ -32,7 +34,6 @@
                             plrPos = plrPos - diceRoll;
                             Console.WriteLine("Player position can't be >100 hence not updating the position");
                         }
-                        Console.WriteLine();
                         break;
                     case SNAKE:
                         Console.WriteLine("For this dice roll the move is  Snake");
@@ -42,11 +43,13 @@
                             plrPos = MIN_POS;
                             Console.WriteLine("Player position can't be -ve hence resetting to 0");
                         }
-                        Console.WriteLine();
                         break;
                 }
+                Console.WriteLine("Player position updated to: " + plrPos + "\n");
+                countOfRolls++;
             }
-            Console.WriteLine("Player position: " + plrPos + "\n");
+            //Console.WriteLine("Player position: " + plrPos + "\n");
+            Console.WriteLine("Total count of dice rolls: " + countOfRolls);
         }
     }
 }
